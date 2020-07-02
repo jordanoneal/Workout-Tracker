88caea89ff793292e25682febe541ea97ba0df4a
jordan o'neal <joneal6694@gmail.com>
2020-07-01 20:00:00 -0600

    index and workout model

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "enter excericse type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "enter exercise name",
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;

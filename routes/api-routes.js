46e26c40161c33906d33bd2fd60c16105e4fa483
jordan o'neal <joneal6694@gmail.com>
2020-07-07 16:00:33 -0600

    working on routes

---
+++ routes/api-routes.js
@@ -0,0 +1,35 @@
+const Workout = require("../models/workout");
+const router = require("express").Router();
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 // routes
-require("./routes/html-routes.js")(app);
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
     console.log("App running on http://localhost:" + PORT);
